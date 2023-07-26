8ee0ce4627d90f19d371265ea186ba9d89f2af10
padfoot-dev <abish102001@gmail.com>
2023-07-27 00:17:29 +0530

    Tree data structure

--- src/scripts/TreeNode.js
+++ src/scripts/TreeNode.js
@@ -1,14 +1,9 @@
-class TreeNode {
+export class CollectionNode {
 
     constructor(name) {
         this.name = name;
-        this.data = {};
         this.parent = null;
         this.children = [];
-    }
-
-    addData(data) {
-        this.data.data = data;
     }
 
     addNode(node) {
@@ -29,21 +24,29 @@ class TreeNode {
     }
 }
 
-const root = new TreeNode("root");
-root.addData("root data");
-console.log("name: " , root.name , " data: " , root.data , " parent " , root.parent , " children: ", root.children );
+export class DataNode {
+    constructor(name) {
+        this.name = name;
+        this.data = {};
+        this.parent = null;
+    }
 
-const rootChildren1 = new TreeNode("root children");
-root.addNode(rootChildren1);
+    addData(data) {
+        this.data.data = data;
+    }
 
-rootChildren1.addData("root children 1 data");
-console.log("name: " , rootChildren1.name , " data: " , rootChildren1.data , " parent " , rootChildren1.parent , " children: ", rootChildren1.children );
+    getLevel() {
+        let parent = this.parent;
+        let level = 0;
+         
+        while(parent) {
+            level +=1;
+            parent = parent.parent;
+        }
 
-const rootChildren2 = new TreeNode("root children 2");
-root.addNode(rootChildren2);
-
-rootChildren2.addData("root children 2 data");
-console.log("name: " , rootChildren2.name , " data: " , rootChildren2.data , " parent " , rootChildren2.parent , " children: ", rootChildren2.children );
+        return level;
+    }
+}
 
 
 
